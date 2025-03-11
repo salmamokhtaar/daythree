9ce646bb0be1ea71ec2fc4cafd101761ef135662
salmamokhtaar <salmam.mohyadiin@gmail.com>
2025-03-11 14:47:15 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//     appBar: AppBar(
+//       title: const Text('Hello Day Three'),
+//       backgroundColor: Colors.lightBlue[700],
+//       centerTitle: true,
+//     ),
+//     body: Center(
+//       child: Home(),
+
+//     ),
+//   )));
+// }
+
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Text('Hello Salma '
+
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Mobile App Developer"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body:  Mine(),
+        
+      )));
+}
+
+class Mine extends StatelessWidget {
+  const Mine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Mobile and Wb Developer!");
+  }
+}
